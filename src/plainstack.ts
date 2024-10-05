7fc3419d683fcbdf67df4b57cabb189e3cbc48b9
joseferben <josef@erben.systems>
2024-10-05 11:21:44 +0200

    fix: export all env vars

--- src/plainstack.ts
+++ src/plainstack.ts
@@ -1,6 +1,6 @@
 export { store } from "./entity";
 export { migrate, rollback } from "./database";
-export { test } from "./env";
+export { test, dev, prod } from "./env";
 export { session } from "./middleware/session";
 export { job, queue, perform, work } from "./job";
 export { form } from "./form";
